42e3ab4ec92a85628723e0bff99c7a738900f77b
Joaovictortav <joaojv497@hotmail.com>
2021-10-11 00:03:50 -0300

    Ajustes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  name: 'prod',
+
+  SOCKET_ENDPOINT: 'https://project-iot-test.herokuapp.com',
+	API_URL: 'https://project-iot-test.herokuapp.com',
 };
